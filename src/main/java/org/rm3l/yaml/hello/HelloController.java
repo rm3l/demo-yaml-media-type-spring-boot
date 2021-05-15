b0962ab8a6d199e95a5f2d556ca5f57f1abe5636
Armel Soro <armel@rm3l.org>
2021-05-15 19:58:19 +0100

    Add support for YAML by manually updating Controllers

--- src/main/java/org/rm3l/yaml/hello/HelloController.java
+++ src/main/java/org/rm3l/yaml/hello/HelloController.java
@@ -1,5 +1,9 @@
 package org.rm3l.yaml.hello;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.IOException;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +22,16 @@ class HelloController {
   @ResponseBody
   public HelloRequest.Response sayHello(@RequestBody final HelloRequest request) {
     return this.helloService.sayHello(request);
+  }
+
+  @PostMapping(value = "/hello", consumes = {"application/yaml", "application/yml"})
+  @ResponseBody
+  public HelloRequest.Response sayHelloFromYaml(@RequestBody final Resource resource)
+      throws IOException {
+    try (final var inputStream = resource.getInputStream()) {
+      return this.sayHello(new ObjectMapper(new YAMLFactory())
+          .readValue(inputStream, HelloRequest.class));
+    }
   }
 
 }
