0909e8cf3a5cf19675edf7150ebe2396c5a48ecd
Armel Soro <armel@rm3l.org>
2021-05-15 14:58:19 +0100

    Initial commit

---
+++ src/main/java/org/rm3l/demoyamlmediatypespringboot/DemoYamlMediaTypeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.rm3l.demoyamlmediatypespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYamlMediaTypeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoYamlMediaTypeSpringBootApplication.class, args);
+	}
+
+}
